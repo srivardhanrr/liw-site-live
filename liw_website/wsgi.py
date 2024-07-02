74e57c2d2cd43806158174c6e21ffb68e89fc66f
Srivardhan <srivardhan.singh.rathore@gmail.com>
2024-07-02 12:45:44 +0530

    first commit

---
+++ liw_website/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for liw_website project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'liw_website.settings')
+
+application = get_wsgi_application()
