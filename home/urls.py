c1152b47d6ac12d1150426d5a9916bfb475d4e51
Srivardhan <srivardhan.singh.rathore@gmail.com>
2024-08-17 01:28:35 +0530

    first commit

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path('/contact', views.contact, name='contact'),
+    path('/symposium', views.symposium, name='symposium'),
 ]
--- home/views.py
+++ home/views.py
@@ -4,3 +4,10 @@ from django.shortcuts import render
 def home(request):
     return render(request, "home/index.html")
 
+
+def contact(request):
+    return render(request, "home/contact.html")
+
+
+def symposium(request):
+    return render(request, "home/symposium.html")
